91a16ee58536df08c1b3a3806502b699ad426003
jcosses <120616033+jcosses@users.noreply.github.com>
2023-05-24 13:08:40 -0400

    Código Python sin Validar
    Nos falta Validar código con herramienta try-except

---
+++ CLAS_11/Ejemplo1.py
@@ -0,0 +1,11 @@
+# Código de ejemplos de ingreso datos
+
+print("EJEMPLOS DE CODIGO EN PYTHON")
+print("---------------------------------\n")
+vNom = input("Ingrese su Nombre: ")
+vEdad = int(input("Ingrese su Edad"))
+print("==============================")
+print(f"Su Nombre es: {vNom}")
+print(f"Su Edad es: {vEdad}")
+
+print("Programa Finalizado")
